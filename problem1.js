3fdbe697e7c17b3002b277bfdb66e6807f8dd90f
fiveamcoder <mahmudcodes@gmail.com>
2024-02-03 09:32:11 +0600

    free water

---
+++ problem1.js
@@ -0,0 +1,13 @@
+/* 
+Free Water
+    - Burger more than 500tk: free Water
+    - Else Water: 30tk
+*/
+
+var burger = 500;
+
+if(burger > 500){
+    console.log("you get free water");
+}else{
+    console.log("Water price 30 tk");
+}
